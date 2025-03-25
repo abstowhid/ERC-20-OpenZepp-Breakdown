d633afd8a56245e76fed1664f5b0dbf08620aead
Towhid <blatowhid@gmail.com>
2025-03-25 21:15:50 +0600

    Update 6_spend-allowence.sol

--- 6_spend-allowence.sol
+++ 6_spend-allowence.sol
@@ -1,4 +1,5 @@
-//this function control the transaxtion(allowence) for the child(spender) under a parent.
+//this function control the transaxtion limit(allowence) for the child(spender) under a parent.
+//it updates the limit & check the limit. if limit is now not transfeerable then spender can't transaction.
 function _spendAllowance(address owner, address spender, uint256 value) internal {
     uint256 currentAllowance = allowances[owner][spender];
     require(currentAllowance >= value, "Insufficient allowance");
