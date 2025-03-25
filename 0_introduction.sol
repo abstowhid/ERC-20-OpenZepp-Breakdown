ebee6814bd1daaf637019d64582a7d8c241bdff6
Towhid <blatowhid@gmail.com>
2025-03-25 15:44:39 +0600

    Update 0_introduction.sol

--- 0_introduction.sol
+++ 0_introduction.sol
@@ -1,1 +1,8 @@
-//will be added later
+there are serveral nesting like one function call in another function.
+
+1.for transfer
+     i. transfer function is just for calling _transfer and providing the details.
+    ii._transfer function checking input and calling _update function , where the real operations . 
+    iii. _update function is updating the values by checking all conditions
+
+
