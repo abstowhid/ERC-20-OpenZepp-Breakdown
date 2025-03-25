7d79e75056d5352f8f5293638f4afa75a60a292e
Towhid <blatowhid@gmail.com>
2025-03-25 15:31:12 +0600

    Update 1_key_components.sol

--- 1_key_components.sol
+++ 1_key_components.sol
@@ -6,3 +6,4 @@ string private _symbol;
 
 
 these are key components(state variable) to set those state variable we will use constructo
+//by constructor we will add those name symbol total suply for a basic token (that always need)
