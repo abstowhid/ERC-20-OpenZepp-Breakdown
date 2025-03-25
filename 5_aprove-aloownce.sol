8a547c07d7cf42166f6cb994c816500f3e2558cf
Towhid <blatowhid@gmail.com>
2025-03-25 21:29:40 +0600

    Update 5_aprove-aloownce.sol

--- 5_aprove-aloownce.sol
+++ 5_aprove-aloownce.sol
@@ -1,2 +1,14 @@
 //alowence to view
 //aprove to set
+function _approve(address owner, address spender, uint256 value, bool emitEvent) internal virtual {
+    if (owner == address(0)) {
+        revert ERC20InvalidApprover(address(0));
+    }
+    if (spender == address(0)) {
+        revert ERC20InvalidSpender(address(0));
+    }
+    _allowances[owner][spender] = value;
+    if (emitEvent) {
+        emit Approval(owner, spender, value);
+    }
+}
