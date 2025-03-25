7391a6fff23ecb7acb9fdafa51bf0ae60fc30966
Towhid <blatowhid@gmail.com>
2025-03-25 15:28:36 +0600

    Create 1_key_components.sol

---
+++ 1_key_components.sol
@@ -0,0 +1,8 @@
+mapping(address account => uint256) private _balances;
+mapping(address account => mapping(address spender => uint256)) private _allowances;
+uint256 private _totalSupply;
+string private _name;
+string private _symbol;
+
+
+these are key components(state variable) to set those state variable we will use constructo
