64c38f34d41d1d5c74343ef163cc52321bb9bfdc
Towhid <blatowhid@gmail.com>
2025-03-25 15:39:19 +0600

    Create 4_transaction.sol

---
+++ 4_transaction.sol
@@ -0,0 +1,3 @@
+//for a transaction
+1.parent=> we need update function (to increase receiver,decrease sender)
+2.child=> for child(allownce) we need to aprove the amount then(aprove function),=> then aloowence spender function for the child spender transaction =>and also update function.
