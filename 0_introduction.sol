55b5473272985e5f9086f368e527888d8b31e464
Towhid <blatowhid@gmail.com>
2025-03-25 21:57:15 +0600

    Update 0_introduction.sol

--- 0_introduction.sol
+++ 0_introduction.sol
@@ -6,3 +6,18 @@ there are serveral nesting like one function call in another function.
     iii. _update function is updating the values by checking all conditions
 
 
+Note: while calling an another function, i must provide the parameter. where i will use the parameter , there the values of the parameter will work for the function(from where calling{parameter's value function})
+=>suppose
+i have a function with 3 paramter of it's own
+
+function _update(addres from, addres to,uint amount){
+//code
+}
+
+now in another functoin i will call it
+
+function _mint(para1,para2,para3)  {
+_update(para1, para2, para3); 
+//or,i need only para3, others will be auto(like msg.sender,0 add)
+_update(address(0), msg.sender, para3);
+}
