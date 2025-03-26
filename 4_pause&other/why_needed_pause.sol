ccf4447c00bf09ca704268a5a629050831bbee75
Towhid <blatowhid@gmail.com>
2025-03-26 17:40:34 +0600

    Update why_needed_pause.sol

--- 4_pause&other/why_needed_pause.sol
+++ 4_pause&other/why_needed_pause.sol
@@ -1,1 +1,9 @@
 //to pause and unpause this .sol is neded
+//pasue=>
+@openzeppelin/contracts/security/Pausable.sol
+
+
+others upgrade feature=>
+@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol
+
+@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol
